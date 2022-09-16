b59054ffab9adcb68cdb67248a35c9599143cbce
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-16 09:18:49 +0200

    Create basic layout

--- app/src/main/java/com/example/szinek/MainActivity.java
+++ app/src/main/java/com/example/szinek/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.szinek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RelativeLayout layout;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        layout = findViewById(R.id.layout);
+        text = findViewById(R.id.textSzin);
     }
 }
