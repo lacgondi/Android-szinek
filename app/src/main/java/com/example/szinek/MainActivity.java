8b7d0dc15417991b43ed564aa5e8010d07c565c7
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-16 09:33:42 +0200

    Add function that lets you tap the screen and it changes color

--- app/src/main/java/com/example/szinek/MainActivity.java
+++ app/src/main/java/com/example/szinek/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.szinek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private RelativeLayout layout;
     private TextView text;
+    private Random rand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int r = rand.nextInt(256);
+                int g = rand.nextInt(256);
+                int b = rand.nextInt(256);
+                layout.setBackgroundColor(Color.rgb(r, g, b));
+                String colorText = String.format("(%d, %d, %d)",r,g,b);
+                text.setText(colorText);
+            }
+        });
     }
 
     private void init() {
         layout = findViewById(R.id.layout);
         text = findViewById(R.id.textSzin);
+        rand = new Random();
     }
 }
